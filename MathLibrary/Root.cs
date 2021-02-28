c1e2f691272e3160b5a2c16e208eb853166bc30f
Andrey Valchuk <immigrationimmigrant@gmail.com>
2021-02-28 20:16:38 +0300

    Add project files.

---
+++ MathLibrary/Root.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathLibrary
+{
+    public static class Root
+    {
+        public static double Calculate(double x, int y, double precision)
+        {
+            if (x <= 0 || y <= 0 || precision <= 0)
+            {
+                throw new ArgumentException("All arguments must be positive numbers.");
+            }
+
+            double result = x / 5;
+            double prevResult = int.MaxValue;          
+
+            while (Math.Abs(result - prevResult) > precision)
+            {
+                prevResult = result;
+                result -= (RaiseToPower(y) - x) / (y * RaiseToPower(y - 1));
+            }
+
+            return result;
+
+            double RaiseToPower(int n)
+            {
+                double resultOfRaising = 1;
+
+                for (int i = 0; i < n; i++)
+                {
+                    resultOfRaising *= result;
+                }
+
+                return resultOfRaising;
+            }
+        }
+
+        public static (double, double) CalculateWithComparison(double x, int y, double precision)
+        {
+            double resultFromCustomMethod = Calculate(x, y, precision);
+            double resultFromMathPow = Math.Pow(x, 1d / y);
+
+            return (resultFromCustomMethod, resultFromMathPow);
+        }
+    }
+}
---
+++ RootCalculator/Program.cs
@@ -0,0 +1,53 @@
+﻿using MathLibrary;
+using System;
+
+namespace RootCalculator
+{
+    class Program
+    {
+        static void Main()
+        {
+            double x;
+            int y;
+            double precision;
+
+            Console.Write("Введите положительное число, из которого будет извлекаться корень n-ой степени: ");
+            while(!Double.TryParse(Console.ReadLine(), out x) || x <= 0)
+            {
+                Console.Write("Неверный ввод. Попробуйте еще: ");
+            }
+
+            Console.Write("Введите положительное число n: ");
+            while (!Int32.TryParse(Console.ReadLine(), out y) || y <= 0)
+            {
+                Console.Write("Неверный ввод. Попробуйте еще: ");
+            }
+
+            Console.Write("Точность вычисления: ");
+            while (!Double.TryParse(Console.ReadLine(), out precision) || precision <= 0)
+            {
+                Console.Write("Неверный ввод. Попробуйте еще: ");
+            }
+
+            Console.WriteLine("Введите 1 для нахождения корня n-ой степени из числа.");
+            Console.WriteLine("Введите 2 для нахождения корня n-ой степени из числа и сравнения результатов.");
+
+            switch (Console.ReadLine())
+            {
+                case "1":
+                    Console.WriteLine($"Корень степени {y} из числа {x} равен {Root.Calculate(x, y, precision)}.");
+                    break;
+
+                case "2":
+                    (double resultFromCustomMethod, double resultFromMathPow) = Root.CalculateWithComparison(x, y, precision);
+                    Console.WriteLine($"Корень степени {y} из числа {x} равен {resultFromCustomMethod}.");
+                    Console.WriteLine($"Результат Math.Pow: {resultFromMathPow}.");
+                    break;
+
+                default:
+                    Console.WriteLine("Такой команды нет!!!");
+                    break;
+            }
+        }
+    }
+}
